b29d5ac3884a5355c6a8cddf0cc5a623e3c0e0b8
Nomik94 <kill0374@naver.com>
2022-12-19 17:46:21 +0900

    feat:로깅 메세지

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const cartsRouter = require('./routes/carts');
 const connect = require('./schemas');
 connect();
 
-app.use(express.json());
-app.use('/api', [goodsRouter, cartsRouter]);
+app.use(express.json()); // post, put 전달된 body 데이터를 req.body로 사용할 수 있도록 만든 bodyparser
+
+app.use((req, res, next) => {
+  console.log('Request URL:', req.originalUrl, ' - ', new Date());
+  next();
+});
+
+app.use('/api', [goodsRouter, cartsRouter]); // API가 사용되기 위한 라우터를 등록
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
